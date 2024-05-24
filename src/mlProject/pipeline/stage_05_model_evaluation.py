52e7ebed04d00361a69dbdf68ad5be6e0f4e86cf
Minali0 <minali.jain@seaflux.tech>
2024-05-24 13:24:03 +0530

    model evaluation added

--- src/mlProject/pipeline/stage_05_model_evaluation.py
+++ src/mlProject/pipeline/stage_05_model_evaluation.py
@@ -1,25 +1,25 @@
 from mlProject.config.configuration import ConfigurationManager
-from mlProject.components.model_trainer import ModelTrainer
+from mlProject.components.model_evaluation import ModelEvaluation
 from mlProject import logger
 from pathlib import Path
 
-STAGE_NAME = "Model trainer stage"
+STAGE_NAME = "Model evaluation stage"
 
-class ModelTrainerTrainingPipeline:
+class ModelEvaluationTrainingPipeline:
     def __init__(self):
         pass
 
     def main(self):
         config = ConfigurationManager()
-        model_trainer_config = config.get_model_trainer_config()
-        model_trainer_config = ModelTrainer(config=model_trainer_config)
-        model_trainer_config.train()
+        model_evaluation_config = config.get_model_evaluation_config()
+        model_evaluation_config = ModelEvaluation(config=model_evaluation_config)
+        model_evaluation_config.save_results()
 
 
 if __name__ == '__main__':
     try:
         logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-        obj = ModelTrainerTrainingPipeline()
+        obj = ModelEvaluationTrainingPipeline()
         obj.main()
         logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
     except Exception as e:
